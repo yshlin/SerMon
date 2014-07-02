910721328779ca8c620b63cb1c17658ba22e15f5
Eddie Lin <yshlin@gmail.com>
2014-07-02 14:28:37 +0800

    Removed android generated files, added missing config file.

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/yshlin/Source/sermon/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/yshlin/Source/sermon/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/SerMon-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/yshlin/Source/sermon/platforms/android/ant-build/SerMon-debug-unaligned.apk : \
-/Users/yshlin/Source/sermon/platforms/android/ant-build/SerMon.ap_ \
-/Users/yshlin/Source/sermon/platforms/android/ant-build/classes.dex \
-/Users/yshlin/Source/sermon/platforms/android/CordovaLib/ant-build/classes.jar \
